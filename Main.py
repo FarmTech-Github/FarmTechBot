877260b40c2a71fb0a0b6a23ddebae9af62cc9c6
Johan Sebastian <65955878+JohanSanSebastian@users.noreply.github.com>
2021-03-09 11:20:52 +0400

    Small modification 🎨

--- Main.py
+++ Main.py
@@ -504,58 +504,5 @@ async def help(ctx):
         text=f"{ctx.guild.name}  •  {datetime.strftime(datetime.now(), '%d.%m.%Y at %I:%M %p')}")
     await ctx.send(embed=help_embed)
 
-
-@client.command()
-async def server(ctx):
-    """Shows server info"""
-
-    server = ctx.guild
-
-    roles = str(len(server.roles))
-    emojis = str(len(server.emojis))
-    channels = str(len(server.channels))
-
-    embeded = discord.Embed(
-        title=server.name, description='Server Info', color=discord.Colour.blurple())
-    embeded.set_thumbnail(url=server.icon_url)
-    embeded.add_field(name="Created on:", value=server.created_at.strftime(
-        '%d %B %Y at %H:%M UTC+3'), inline=False)
-    embeded.add_field(name="Server ID:", value=server.id, inline=False)
-    embeded.add_field(name="Users on server:",
-                      value=server.member_count, inline=True)
-    embeded.add_field(name="Server owner:", value=server.owner, inline=True)
-
-    embeded.add_field(name="Server Region:", value=server.region, inline=True)
-    embeded.add_field(name="Verification Level:",
-                      value=server.verification_level, inline=True)
-
-    embeded.add_field(name="Role Count:", value=roles, inline=True)
-    embeded.add_field(name="Emoji Count:", value=emojis, inline=True)
-    embeded.add_field(name="Channel Count:", value=channels, inline=True)
-
-    await ctx.send(embed=embeded)
-
-
-@client.command()
-async def user(ctx, *, user: discord.Member = None):
-
-    if user is None:
-        user = ctx.author
-
-    embed = discord.Embed(
-        colour=discord.Colour.dark_green(),
-        title=f"{user.name}'s Stats and Information."
-    )
-    embed.set_footer(text=f"ID: {user.id}")
-    embed.set_thumbnail(url=user.avatar_url_as(format="png"))
-    embed.add_field(name="__**General information:**__", value=f"**Discord Name:** {user.name}\n"
-                                                               f"**Account created:** {user.created_at.__format__('%A %d %B %Y at %H:%M')}\n"
-                                                               f"**Status:** {user.status}\n"
-                                                               f"**Activity:** {user.activity}", inline=False)
-    embed.add_field(name="__**Server-related information:**__", value=f"**Nickname:** {user.nick}\n"
-                                                                      f"**Joined server:** {user.joined_at.__format__('%A %d %B %Y at %H:%M')}\n"
-                                                                      f"**Roles:** {' '.join([r.mention for r in user.roles[1:]])}")
-    return await ctx.send(embed=embed)
-
 ######################################### Client Run ########################################
 client.run(token)
